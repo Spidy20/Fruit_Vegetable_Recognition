cfaa8a9e1c35a672a61e2bea599f1ffe0f3d982d
Kushal Bhavsar <kushalbhavsar58@gmail.com>
2021-11-19 09:09:16 +0530

    Initial commit

---
+++ Fruits_Vegetable_Classification.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from PIL import Image
+from keras.preprocessing.image import load_img,img_to_array
+import numpy as np
+from keras.models import load_model
+import requests
+from bs4 import BeautifulSoup
+
+model = load_model('FV.h5')
+labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot', 7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger', 14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple', 26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn', 32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+
+fruits = ['Apple','Banana','Bello Pepper','Chilli Pepper','Grapes','Jalepeno','Kiwi','Lemon','Mango','Orange','Paprika','Pear','Pineapple','Pomegranate','Watermelon']
+vegetables = ['Beetroot','Cabbage','Capsicum','Carrot','Cauliflower','Corn','Cucumber','Eggplant','Ginger','Lettuce','Onion','Peas','Potato','Raddish','Soy Beans','Spinach','Sweetcorn','Sweetpotato','Tomato','Turnip']
+
+def fetch_calories(prediction):
+    try:
+        url = 'https://www.google.com/search?&q=calories in ' + prediction
+        req = requests.get(url).text
+        scrap = BeautifulSoup(req, 'html.parser')
+        calories = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
+        return calories
+    except Exception as e:
+        st.error("Can't able to fetch the Calories")
+        print(e)
+
+def processed_img(img_path):
+    img=load_img(img_path,target_size=(224,224,3))
+    img=img_to_array(img)
+    img=img/255
+    img=np.expand_dims(img,[0])
+    answer=model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
+
+def run():
+    st.title("Fruits🍍-Vegetable🍅 Classification")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250,250))
+        st.image(img,use_column_width=False)
+        save_image_path = './upload_images/'+img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+
+        # if st.button("Predict"):
+        if img_file is not None:
+            result= processed_img(save_image_path)
+            print(result)
+            if result in vegetables:
+                st.info('**Category : Vegetables**')
+            else:
+                st.info('**Category : Fruit**')
+            st.success("**Predicted : "+result+'**')
+            cal = fetch_calories(result)
+            if cal:
+                st.warning('**'+cal+'(100 grams)**')
+run()
---
+++ README.md
@@ -0,0 +1,38 @@
+# Fruits🍍-Vegetable🍅 Classification
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)                 
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)   
+
+## [Watch Tutorial for this project](https://youtu.be/HTSDryllx0Y)
+<img src="https://github.com/Spidy20/Text_to_Handwritten_Text_Converter/blob/master/yt_thumb.jpg">
+
+## Usage:-
+
+- Clone my repository.
+- Open CMD in working directory.
+- Run following command.
+
+  ```
+  pip install -r requirements.txt
+  ```
+- `Fruits_Vegetable_Classification.py` is the main Python file of Streamlit Web-Application. 
+- `Fruit_Veg_Classification_Mobilenet.ipynb` is the Notebook file of the Training
+- Dataset that I have used is [Fruit and Vegetable Image Recognition](https://www.kaggle.com/kritikseth/fruit-and-vegetable-image-recognition).
+- To run app, write following command in CMD. or use any IDE.
+
+  ```
+  streamlit run Fruits_Vegetable_Classification.py
+  ```
+
+- For more explanation of this project see the tutorial on Machine Learning Hub YouTube channel.
+
+## Screenshots
+
+<img src="https://github.com/Spidy20/Text_to_Handwritten_Text_Converter/blob/master/t1.png">
+<img src="https://github.com/Spidy20/Text_to_Handwritten_Text_Converter/blob/master/t2.PNG">
+
+
+## Just follow☝️ me and Star⭐ my repository 
+
+# [Buy me a Coffee☕](https://www.buymeacoffee.com/spidy20)
+## [Donate me on PayPal(It will inspire me to do more projects)](https://www.paypal.me/spidy1820)
