e2ba094c52fdf0c1d59255dab69bfc67ce115c07
Kushal Bhavsar <kushalbhavsar58@gmail.com>
2022-12-07 10:44:29 +0530

    EC2 API Added

---
+++ ec2_api.py
@@ -0,0 +1,48 @@
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from flask import Flask, jsonify, request
+
+model = load_model('FV.h5')
+
+labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
+          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
+          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
+          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
+          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+
+
+def prepare_image(img_path):
+    img = load_img("./upload_images/test.jpg", target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
+
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    if 'file' not in request.files:
+        return jsonify(error="Please try again. The Image doesn't exist")
+
+    file = request.files.get('file')
+    img_bytes = file.read()
+    with open("./upload_images/test.jpg", "wb") as img:
+        img.write(img_bytes)
+    result = prepare_image(img_bytes)
+    return jsonify(prediction=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
